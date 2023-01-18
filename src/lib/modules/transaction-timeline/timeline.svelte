5deaee94265f0df8917b3a34fdd6fe432acdf346
KyleTruong <ktruong008@gmail.com>
2023-01-18 10:52:56 -0500

    Timeline UI update

--- src/lib/modules/transaction-timeline/timeline.svelte
+++ src/lib/modules/transaction-timeline/timeline.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import TimelineDivider from './timeline-divider.svelte';
+  import TransactionsBlock from './transactions-block.svelte';
 </script>
 
 <div class="flow-root">
@@ -12,35 +13,31 @@
         />
         <div class="relative flex items-start space-x-3">
           <div class="relative">
-            <!-- <img
-              class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400 ring-8 ring-white"
-              src="https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-              alt=""
-            /> -->
             <div
               class="flex w-10 items-center justify-center rounded-full bg-white ring-0"
             >
               <TimelineDivider extraClasses="text-gray-500" />
             </div>
           </div>
-          <div class="min-w-0 flex-1">
-            <div>
-              <div class="text-sm">
-                <a
-                  href="#"
-                  class="font-medium text-gray-900">Eduardo Benz</a
-                >
-              </div>
-              <p class="mt-0.5 text-sm text-gray-500">Commented 6d ago</p>
-            </div>
-            <div class="mt-2 text-sm text-gray-700">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc
-                ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam
-                cursus quis sed purus nam.
-              </p>
+          <TransactionsBlock />
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="relative pb-8">
+        <span
+          class="absolute top-5 left-5 -ml-px h-full w-0.5 bg-gray-200"
+          aria-hidden="true"
+        />
+        <div class="relative flex items-start space-x-3">
+          <div class="relative">
+            <div
+              class="flex w-10 items-center justify-center rounded-full bg-white ring-0"
+            >
+              <TimelineDivider extraClasses="text-gray-500" />
             </div>
           </div>
+          <TransactionsBlock />
         </div>
       </div>
     </li>
