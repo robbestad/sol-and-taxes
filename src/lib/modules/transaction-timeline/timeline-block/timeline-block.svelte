55b6cd41b6fe11908d0b43b8cf7379ea17666b56
KyleTruong <ktruong008@gmail.com>
2023-01-18 16:27:05 -0500

    Add hide/show on blocks

--- src/lib/modules/transaction-timeline/timeline-block/timeline-block.svelte
+++ src/lib/modules/transaction-timeline/timeline-block/timeline-block.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
   import { DateTime } from 'luxon';
+  import { slide } from 'svelte/transition';
 
   import GitCommitIcon from '$lib/shared/icons/git-commit-icon.svelte';
+  import HorizontalRuleIcon from '$lib/shared/icons/horizontal-rule-icon.svelte';
+
   import TransactionsBlock from './transactions-block.svelte';
 
   export let dailyBlock;
 
-  // https://daveceddia.com/svelte-reactive-destructuring/
-  // $: ({ isoDate, transactions } = dailyBlock);
-  const { isoDate, transactions } = dailyBlock;
+  let isBlockHidden = false;
 
-  const formattedDate = DateTime.fromISO(isoDate).toLocaleString(DateTime.DATE_MED);
+  $: ({ isoDate, transactions } = dailyBlock);
+  $: formattedDate = DateTime.fromISO(isoDate).toLocaleString(DateTime.DATE_MED);
 
-  // $: {
-  //   console.log('')
-  //   console.log('isoDate', isoDate);
-  //   console.log('DateTime.fromISO(isoDate): ', DateTime.fromISO(isoDate));
-  //   console.log('DateTime.fromISO(isoDate).toLocaleString(DateTime.DATE_MED): ', DateTime.fromISO(isoDate).toLocaleString(DateTime.DATE_MED));
-  //   console.log('formattedDate: ', formattedDate);
-  // }
+  const toggleBlockVisibility = () => {
+    isBlockHidden = !isBlockHidden;
+  };
 </script>
 
 <li>
@@ -33,18 +31,30 @@
     <div class="relative flex items-start space-x-3">
       <!-- Divider icons -->
       <div class="relative">
-        <div class="flex w-10 items-center justify-center rounded-full bg-white ring-0">
-          <GitCommitIcon extraClasses="text-gray-500" />
-          <!-- <HorizontalRuleIcon extraClasses="text-gray-500" /> -->
-        </div>
+        <button
+          on:click={toggleBlockVisibility}
+          class="flex w-10 items-center justify-center rounded-full bg-white ring-0 hover:cursor-pointer"
+        >
+          {#if isBlockHidden}
+            <HorizontalRuleIcon extraClasses="text-gray-500" />
+          {:else}
+            <GitCommitIcon extraClasses="text-gray-500" />
+          {/if}
+        </button>
       </div>
 
       <!-- Cards -->
       <div class="flex flex-col w-full">
         <div class="min-w-0 flex-1 mb-1.5">
-          <div class="text-sm text-gray-500">{formattedDate}</div>
+          <div class="text-sm text-gray-500">
+            Transactions on {formattedDate}
+          </div>
         </div>
-        <TransactionsBlock />
+        {#if !isBlockHidden}
+          <div transition:slide>
+            <TransactionsBlock />
+          </div>
+        {/if}
       </div>
     </div>
   </div>
--- src/lib/modules/transaction-timeline/timeline-block/transactions-block.svelte
+++ src/lib/modules/transaction-timeline/timeline-block/transactions-block.svelte
@@ -2,7 +2,9 @@
   import TransactionCard from './transaction-card.svelte';
 </script>
 
-<div class="min-w-0 flex-1">
+<div
+  class="min-w-0 flex-1"
+>
   <div class="overflow-hidden bg-white shadow sm:rounded-md">
     <ul class="divide-y divide-gray-200">
       <TransactionCard />
