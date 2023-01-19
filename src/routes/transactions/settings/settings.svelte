3e461b5bfc6fefacc62de1c41f0285357828fb17
KyleTruong <ktruong008@gmail.com>
2023-01-19 18:24:55 -0500

    multiselect poc-2

--- src/routes/transactions/settings/settings.svelte
+++ src/routes/transactions/settings/settings.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
-  import MultiSelect from 'svelte-multiselect';
+  import MultiSelect from '$lib/shared/form/multi-select.svelte';
 
-  const ui_libs = [`Svelte`, `React`, `Vue`, `Angular`, `...`];
+  const ui_libs = ['AF', 'SQ', 'AR', 'HY', 'EU'];
+  const bu_libs = ['AF', 'SQ', 'AR', 'HY', 'EU'];
 
-  let selected = [];
+  let selected;
+  let selected2;
 </script>
 
 <div class="pb-5 mb-5 w-full">
-  <div class="mt-3 flex gap-4 sm:mt-0 sm:ml-4">
+  <div class="mt-3 flex flex-col gap-4 sm:mt-0 sm:ml-4">
     <!-- Wallet address -->
     <div>
       <label
@@ -30,11 +32,13 @@
         class="block text-sm font-medium text-gray-700">Transaction types</label
       >
 
-      <MultiSelect
-        bind:selected
-        options={ui_libs}
-        outerDivClass={`mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm`}
-      />
+      <MultiSelect bind:value={selected}>
+        <option value="AF">Afrikanns</option>
+        <option value="SQ">Albanian</option>
+        <option value="AR">Arabic</option>
+        <option value="HY">Armenian</option>
+        <option value="EU">Basque</option>
+      </MultiSelect>
     </div>
 
     <!-- Transaction sources -->
@@ -44,11 +48,13 @@
         class="block text-sm font-medium text-gray-700">Transaction sources</label
       >
 
-      <MultiSelect
-        bind:selected
-        options={ui_libs}
-        outerDivClass={`mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm`}
-      />
+      <MultiSelect bind:value={selected2}>
+        <option value="AF">Afrikanns</option>
+        <option value="SQ">Albanian</option>
+        <option value="AR">Arabic</option>
+        <option value="HY">Armenian</option>
+        <option value="EU">Basque</option>
+      </MultiSelect>
     </div>
   </div>
 </div>
