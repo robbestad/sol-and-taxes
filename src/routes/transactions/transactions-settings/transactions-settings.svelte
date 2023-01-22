0f954d44cc4a70afe956f0486f4e12caf21d76db
KyleTruong <ktruong008@gmail.com>
2023-01-21 22:24:23 -0500

    Add more context to purchasing

--- src/routes/transactions/transactions-settings/transactions-settings.svelte
+++ src/routes/transactions/transactions-settings/transactions-settings.svelte
@@ -175,7 +175,7 @@
       >
         <dd class="mt-1 text-sm tracking-tight text-gray-500">
           Fetching transactions uses 100 credits and returns ~80 transactions each time.
-          1,000 credits costs 0.1 SOL.
+          Purchases may take up to a minute to process, please do not close the page.
         </dd>
         <button
           on:click={buyMoreCredits}
@@ -183,7 +183,7 @@
           type="button"
           class="inline-flex disabled:opacity-50 mt-2 items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
-          Buy 1,000 credits
+          {isBuyingCredits ? `Purchasing...` : `Buy 1,000 credits for 0.1 SOL`}
         </button>
       </div>
     </div>
